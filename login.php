392f66e182f2ea2dd993f0df34b03829720f4676
Eliam LOTONGA <e.m.lotonga@gmail.com>
2018-11-27 15:30:22 +0100

    correction requete sql

--- login.php
+++ login.php
@@ -4,17 +4,17 @@ $username = $_POST['user_name'];
 $user_pass = $_POST["password"];
 $sign=$_POST["s"];
 
-$req=$bdd->prepare("select password from employee_data where username like "joel"");
-$response=$req->execute() or die('Erreur with SQL');
+$req=$bdd->prepare("select password from employee_data where username=:username");
+$response=$req->execute( array(':username'=>$username) ) or die('Erreur with SQL');
 
-while ($row = $req->fetchALL()){
+while ($row = $response->fetchALL()){
 echo $row['password'];
 
 }
-$req = $bdd->prepare("select * from employee_data where username like '$username' and password like '$user_pass' ");
- $response = $req->execute() or die('Erreur lors de execution');
+$req = $bdd->prepare("select * from employee_data where username=:username and password=:user_pass ");
+$response = $req->execute( array(':username'=>$username, ':user_pass'=>$password) ) or die('Erreur lors de execution');
  
-  while ($row = $req->fetchALL()) {
+  while ($row = $response->fetchALL()) {
 	  
 	  if($row>0){
 	 	echo "ok";
